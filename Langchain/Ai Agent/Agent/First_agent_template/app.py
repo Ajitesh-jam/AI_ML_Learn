6f65ad5db1289225bc5169d24d508563cc70332c
Ajitesh Jamulkar <126882431+Ajitesh-jam@users.noreply.github.com>
2025-11-05 23:38:57 +0530

    image process

--- Langchain/Ai Agent/Agent/First_agent_template/app.py
+++ Langchain/Ai Agent/Agent/First_agent_template/app.py
@@ -45,7 +45,7 @@ max_tokens=2096,
 temperature=0.5,
 model_id='Qwen/Qwen2.5-Coder-32B-Instruct',# it is possible that this model may be overloaded
 custom_role_conversions=None,
-token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")  # Use environment variable for security
+token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN") 
 )
 
 
